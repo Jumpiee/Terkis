2177a7aaabf6b7dccc8ea822504553e38349e768
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2026-05-13 23:56:29 +0700

    fix: logo to '/'

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <Image
-      src="/api/media/file/LOGO.png"
-      alt="TERKIS Logo"
-      width={340}
-      height={100}
-      className="h-8 w-auto"
-    />
+    <Link href="/" className="flex items-center gap-2">
+      <Image
+        src="/api/media/file/LOGO.png"
+        alt="TERKIS Logo"
+        width={340}
+        height={100}
+        className="h-8 w-auto"
+      />
+    </Link>
   )
 }
