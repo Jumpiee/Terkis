09e19ddb65c1fd1c83bd13cbec019a2cc7d657fb
N0TAW00D <primsirikunawutnatthawat@gmail.com>
2026-05-29 15:16:34 +0700

    fix: type checkout Page

--- src/components/Cart/CartModal.tsx
+++ src/components/Cart/CartModal.tsx
@@ -108,7 +108,7 @@ export function CartModal() {
                   return (
                     <li className="flex w-full flex-col" key={i}>
                       <div className="relative flex w-full flex-row justify-between px-1 py-4">
-                        <div className="absolute z-40 -mt-2 ml-[55px]">
+                        <div className="absolute z-40 -mt-2 ml-13.75">
                           <DeleteItemButton item={item} />
                         </div>
                         <Link
